523ab1568307b360a52145376ac2b89e010c4038
Ashu Gupta <61074670+ashugupta1@users.noreply.github.com>
2024-10-08 03:04:58 +0530

    add cors url set to *

--- app.js
+++ app.js
@@ -8,17 +8,25 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const MongoDB = require("./config/db");
 
+const allowedOrigins = [
+  "https://to-do-application-frontend-5fhpilz3r-ashugupta1s-projects.vercel.app",
+  "http://localhost:8080", // Add localhost for development
+];
+
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 MongoDB();
 
 // Middleware
-app.use(cors({
-  origin: 'https://to-do-application-frontend-5fhpilz3r-ashugupta1s-projects.vercel.app/',
-  methods: 'GET, POST, PUT, DELETE',
-  credentials: true
-})); // Enable CORS for all origins
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
 app.use(bodyParser.json()); // Parse JSON bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
